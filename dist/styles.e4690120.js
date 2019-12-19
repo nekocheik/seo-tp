43c87cf1c35c5ad777b8183e9bad22ffabc0565c
Cheik <cheik.kone@hetic.net>
2019-12-19 12:09:18 +0100

    modif

--- dist/styles.e4690120.js
+++ dist/styles.e4690120.js
@@ -199,7 +199,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./reset.css":"assets/scss/reset.css","../fonts/stylesheet.css":"assets/fonts/stylesheet.css","./../images/burger__menu.svg":[["burger__menu.6ca2a643.svg","assets/images/burger__menu.svg"],"assets/images/burger__menu.svg"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./reset.css":"assets/scss/reset.css","../fonts/stylesheet.css":"assets/fonts/stylesheet.css","_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -227,7 +227,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54416" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64241" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
